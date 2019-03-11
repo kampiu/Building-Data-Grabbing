90f72594030079d8d1d5da67a0ef155d84b799a7
KAM <1479376017@qq.com>
2019-03-11 20:18:24 +0800

    完善工具类
    新增AJAX请求

--- getBuild.js
+++ getBuild.js
@@ -8,7 +8,8 @@ const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const sequelize = require('./model/sequelize')
 // 导入工具类
-const { dateFormat, getTimestamps, fixInteger } = require('../util/tool')
+const { dateFormat, getTimestamps, fixInteger } = require('./util/tool')
+const { getData } = require('./util/ajax')
 // 导入数据模型
 const AreaModel = require('./model/areaModel')
 const BuildModel = require('./model/buildModel')
@@ -25,7 +26,24 @@ FrameModel.belongsTo(BuildModel, { foreignKey: 'building_id',targetKey: 'id' });
 //
 // Country.hasMany(City, {foreignKey: 'countryCode', sourceKey: 'isoCode'});
 // City.belongsTo(Country, {foreignKey: 'countryCode', targetKey: 'isoCode'});
+// 初始化抓取数据的信息
+let cityId = 441900;        // => 需要抓取城市的ID
+let page = 0;               // => 分页
+let limit = 20;             // => 分页中的每一页的数量
+// 获取到的数据信息
 
+const getBuildList = () => {
+	let Url = `http://app.api.lianjia.com/newhouse/app/feed/index?city_id=${cityId}&has_filter=0&limit_count=${limit}&page=${page}&request_ts=${getTimestamps}`
+	getData({
+		url: Url,
+		method: "POST",
+		json: true,
+		headers: {
+			"content-type": "application/json",
+		},
+		body: JSON.stringify(requestData)
+	})
+}
 
 
 const start = (param) => {
@@ -35,7 +53,7 @@ const start = (param) => {
 		city_id: data.city_id,
 		area_id: data.district_id,
 		banner: data.preload_detail_image[0].image_size_url,
-		opening_time: data.on_time,                         // 时间过滤格式化 YY/MM/DD
+		opening_time: dateFormat(data.on_time),                         // 时间过滤格式化 YY/MM/DD
 		address: data.address,
 		coor: `${data.longitude},${data.latitude}`,
 		average_price: data.average_price,
--- util/ajax.js
+++ util/ajax.js
@@ -9,7 +9,9 @@ const Sequelize = require('sequelize')
 const sequelize = require('./model/sequelize')
 const { dateFormat, getTimestamps, fixInteger } = require('./tool')
 
-const getData = (optionsParam = {}) => {
+const getData = (requestData = {
+	url: '',
+},optionsParam = {}) => {
 	let options = Object.assign({
 		city_id: 441900,
 		has_filter: 0,
@@ -18,6 +20,15 @@ const getData = (optionsParam = {}) => {
 		request_ts: getTimestamps()
 	}, optionsParam)
 
+	return new Promise((resolve, reject) => {
+		request(requestData, (error, response, body) => {
+			if (!error && response.statusCode == 200) {
+				console.log(body)
+			}else{
+				reject({err: error, msg: '请求失败 -> 请检查请求是否正确'})
+			}
+		})
+	})
 }
 
 module.exports = {
